219bf7618f01decd110d6337c9a568c1111d7146
iamkanguk97 <iamkanguk97@kakao.com>
2023-01-16 23:08:16 +0900

    UserFollow 테이블 status 컬럼 제거

--- src/api/models/User/UserFollow.js
+++ src/api/models/User/UserFollow.js
@@ -20,12 +20,6 @@ module.exports = class Follow extends Sequelize.Model {
                 allowNull: false,
                 comment: '팔로우를 당하는 사용자 고유값'  
             },
-            FOLLOW_STATUS: {
-                type: Sequelize.CHAR(1),
-                allowNull: false,
-                defaultValue: 'Y',
-                comment: '팔로우 상태 (Y: 활성화, N: 비활성화)'
-            },
             CREATED_AT: {
                 type: 'TIMESTAMP',
                 allowNull: false,
