c6a5a5cbcb664d041b46cf10582de0bf2d4013a1
iamkanguk97 <iamkanguk97@kakao.com>
2023-01-27 20:53:03 +0900

    User 관련 Model Swagger Schema 추가

--- src/api/models/User/User.js
+++ src/api/models/User/User.js
@@ -1,5 +1,68 @@
 const Sequelize = require('sequelize');
 
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          User:
+ *              type: object
+ *              required:
+ *                  - IDX
+ *                  - USER_NICKNAME
+ *                  - USER_KAKAO_ID
+ *                  - USER_STATUS
+ *                  - USER_PROVIDER
+ *                  - CREATED_AT
+ *                  - UPDATED_AT
+ *              properties:
+ *                  IDX:
+ *                      type: integer
+ *                      description: '사용자 고유값'
+ *                  USER_EMAIL:
+ *                      type: string
+ *                      format: email
+ *                      description: '사용자 이메일 (카카오에서 받아오는 이메일)'
+ *                  USER_NICKNAME:
+ *                      type: string
+ *                      description: '사용자 닉네임 (회원가입 시 설정)'
+ *                  USER_PROFILE_IMAGE:
+ *                      type: string
+ *                      description: '사용자 프로필 이미지 링크 (S3 링크)'
+ *                  USER_KAKAO_ID:
+ *                      type: string
+ *                      description: '사용자 카카오 고유값'
+ *                  USER_AGE_GROUP:
+ *                      type: string
+ *                      description: '사용자 연령대'
+ *                  USER_GENDER:
+ *                      type: char
+ *                      description: '사용자 성별'
+ *                  USER_STATUS:
+ *                      type: char
+ *                      description: '사용자 상태 (A: 활성화됨, D: 탈퇴됨)'
+ *                  USER_PROVIDER:
+ *                      type: char
+ *                      description: '사용자 로그인 타입 (K: 카카오)'
+ *                  CREATED_AT:
+ *                      type: timestamp
+ *                      description: '등록 일자'
+ *                  UPDATED_AT:
+ *                      type: timestamp
+ *                      description: '수정 일자'
+ *              example:
+ *                  IDX: 1
+ *                  USER_EMAIL: 'iamkanguk97@kakao.com'
+ *                  USER_NICKNAME: '욱이'
+ *                  USER_PROFILE_IMAGE: 'profileImage.com'
+ *                  USER_KAKAO_ID: '123456678123'
+ *                  USER_AGE_GROUP: '20대'
+ *                  USER_GENDER: 'M'
+ *                  USER_STATUS: 'A'
+ *                  USER_PROVIDER: 'K'
+ *                  CREATED_AT: '2022-01-01 13:00:00'
+ *                  UPDATED_AT: '2022-01-01 13:00:00'
+ */
+
 module.exports = class User extends Sequelize.Model {
     static init(sequelize) {
         return super.init({
--- src/api/models/User/UserFollow.js
+++ src/api/models/User/UserFollow.js
@@ -1,5 +1,41 @@
 const Sequelize = require('sequelize');
 
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          UserFollow:
+ *              type: object
+ *              required:
+ *                  - IDX
+ *                  - USER_IDX
+ *                  - FOLLOW_TARGET_IDX
+ *                  - CREATED_AT
+ *                  - UPDATED_AT
+ *              properties:
+ *                  IDX:
+ *                      type: integer
+ *                      description: '사용자 팔로우 고유값'
+ *                  USER_IDX:
+ *                      type: integer
+ *                      description: '사용자 고유값'
+ *                  FOLLOW_TARGET_IDX:
+ *                      type: integer
+ *                      description: '팔로우를 당하는 사용자 고유값'
+ *                  CREATED_AT:
+ *                      type: timestamp
+ *                      description: '등록 일자'
+ *                  UPDATED_AT:
+ *                      type: timestamp
+ *                      description: '수정 일자'
+ *              example:
+ *                  IDX: 1
+ *                  USER_IDX: 1
+ *                  FOLLOW_TARGET_IDX: 2 
+ *                  CREATED_AT: '2022-01-01 13:00:00'
+ *                  UPDATED_AT: '2022-01-01 13:00:00'
+ */
+
 module.exports = class Follow extends Sequelize.Model {
     static init(sequelize) {
         return super.init({
