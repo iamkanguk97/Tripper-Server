a8658e0f6729701b7770d61fbe1e821d00ac367f
KANGUK LEE <iamkanguk97@kakao.com>
2023-02-09 23:21:30 +0900

    errorHandleMiddleware 수정

--- src/api/middlewares/errorHandleMiddleware.js
+++ src/api/middlewares/errorHandleMiddleware.js
@@ -2,9 +2,9 @@
 const httpStatus = require('http-status');
 const jwt = require('jsonwebtoken');
 const responseMessage = require('../../config/response/baseResponseStatus');
+const { BadRequestError, ServerError } = require('../utils/errors');
 
 const errorHandleMiddleware = (error, req, res, next) => {
-    console.log(error.constructor.name);
     const err = {
         statusCode: error.statusCode || httpStatus.INTERNAL_SERVER_ERROR,
         // statusCode: error.statusCode || httpStatus.INTERNAL_SERVER_ERROR,
@@ -18,15 +18,16 @@ const errorHandleMiddleware = (error, req, res, next) => {
     }
 
     // WrapAsync로부터 들어온 Error는 따로 객체 만들어서 처리 (Async Error)
-    // if (error instanceof Error) {
-    //     return res.status(err.statusCode).json({
-    //         ...(err.message),
-    //         error: {
-    //             message: error.message,
-    //             stack: error.stack
-    //         }
-    //     })
-    // }
+    // instanceof Error로 하게 되면 모든 error를 감지하기 때문에 현재로서는 Custom Error가 아니면 해당 조건문으로 들어오게 함.
+    if (!(error instanceof BadRequestError || error instanceof ServerError)) {
+        return res.status(err.statusCode).json({
+            ...(err.message),
+            error: {
+                message: error.message,
+                stack: error.stack
+            }
+        })
+    }
     
     // 이외의 Error들 처리
     return res.status(err.statusCode).json(err.message);
