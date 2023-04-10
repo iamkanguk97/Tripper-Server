6e39721be817b4a8ccbcac2fb02f81141fdd63ff
KANGUK LEE <iamkanguk97@kakao.com>
2023-04-11 00:38:46 +0900

    jwtMiddleware 수정

--- src/api/middlewares/jwtMiddleware.js
+++ src/api/middlewares/jwtMiddleware.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET_KEY } = require('../../config/vars');
+const { JWT } = require('../../config/vars');
 const { JWTError } = require('../errors/index');
 
 // JWT 인증 미들웨어
 const jwtMiddleware = (req, res, next) => {
     try {
         const token = req.headers.authorization.split('Bearer ')[1];
-        req.verifiedToken = jwt.verify(token, JWT_SECRET_KEY);
+        req.verifiedToken = jwt.verify(token, JWT.ACCESS_SECRET_KEY);
         return next();
     } catch (err) {
         throw new JWTError(err);
